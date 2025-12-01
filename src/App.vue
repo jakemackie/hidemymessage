678c2f96a68efe61a1383f4c22c0dc59b945b4f2
Jake Mackie <jake.wmackie@gmail.com>
2025-12-01 23:14:13 +0000

    hotfix

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import 'vue-sonner/style.css';
 <template>
   <main class="relative pt-20 sm:pt-0 min-h-screen flex flex-col items-center sm:justify-center">
     <RouterView />
-    <div class="absolute -z-10 inset-0 bg-[radial-gradient(circle,black_1px,transparent_1px)] bg-[length:10px_10px] opacity-10"></div>
+    <div class="absolute -z-10 inset-0 bg-[radial-gradient(circle,black_1px,transparent_1px)] bg-size-[10px_10px] opacity-10"></div>
   </main>
   <Toaster position="top-center" richColors />
 </template>
