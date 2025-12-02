c124e3cd3edd4ad576c4ca6e3439683060ef4045
Jake Mackie <jake.wmackie@gmail.com>
2025-12-02 14:32:25 +0000

    amend: fade out background dots toward the middle

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import 'vue-sonner/style.css';
 <template>
   <main class="relative pt-4 sm:pt-0 min-h-screen flex flex-col items-center sm:justify-center">
     <RouterView />
-    <div class="absolute -z-10 inset-0 bg-[radial-gradient(circle,black_1px,transparent_1px)] bg-size-[10px_10px] opacity-10"></div>
+    <div class="absolute inset-0 -z-10 bg-[radial-gradient(circle,black_1px,transparent_1px)] bg-size-[10px_10px] opacity-10 mask-[radial-gradient(circle,transparent_0%,black_60%)] [-webkit-mask-image:radial-gradient(circle,transparent_0%,black_60%)]"></div>
   </main>
   <Toaster position="top-center" richColors />
 </template>
