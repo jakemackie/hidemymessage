95e0c2b385dc5ea24b5f4272619ecbc4c3ad0465
Jake Mackie <jake@hiyield.co.uk>
2025-12-02 09:51:04 +0000

    amend: less mob padding

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import 'vue-sonner/style.css';
 </script>
 
 <template>
-  <main class="relative pt-20 sm:pt-0 min-h-screen flex flex-col items-center sm:justify-center">
+  <main class="relative pt-4 sm:pt-0 min-h-screen flex flex-col items-center sm:justify-center">
     <RouterView />
     <div class="absolute -z-10 inset-0 bg-[radial-gradient(circle,black_1px,transparent_1px)] bg-size-[10px_10px] opacity-10"></div>
   </main>
